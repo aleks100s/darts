3ef95042ce59d055048cb653d0a924f620ede023
aleks100s <aleksandr100s@yandex.ru>
2023-03-18 16:39:47 +0200

    [iOS] Show NoDataView in statistics

--- iosApp/iosApp/Statistics/ShotDistribution/ShotDistributionView.swift
+++ iosApp/iosApp/Statistics/ShotDistribution/ShotDistributionView.swift
@@ -4,20 +4,29 @@ internal struct ShotDistributionView: View {
 	@StateObject var viewModel: IOSShotDistributionViewModel
 	
 	var body: some View {
-		ScrollView {
-			VStack(spacing: 32) {
-				ZStack {
-					Pie(slices: $viewModel.data)
-					Text("throws_count \(viewModel.totalCount)")
-				}
-				ChartLegend(legend: $viewModel.legend)
-			}
-		}
+		content
 			.onAppear {
 				viewModel.startObserving()
 			}
 			.onDisappear {
 				viewModel.dispose()
 			}
+	}
+	
+	@ViewBuilder
+	private var content: some View {
+		if viewModel.totalCount == 0 {
+			NoDataView()
+		} else {
+			ScrollView {
+				VStack(spacing: 32) {
+					ZStack {
+						Pie(slices: $viewModel.data)
+						Text("throws_count \(viewModel.totalCount)")
+					}
+					ChartLegend(legend: $viewModel.legend)
+				}
+			}
+		}
 	}
 }
--- iosApp/iosApp/Statistics/VictoryDistribution/VictoryDistributionView.swift
+++ iosApp/iosApp/Statistics/VictoryDistribution/VictoryDistributionView.swift
@@ -5,20 +5,29 @@ internal struct VictoryDistributionView: View {
 	@StateObject var viewModel: IOSVictoryDistributionViewModel
 	
 	var body: some View {
-		ScrollView {
-			VStack(spacing: 32) {
-				ZStack {
-					Pie(slices: $viewModel.data)
-					Text("games_count \(viewModel.totalCount)")
-				}
-				ChartLegend(legend: $viewModel.legend)
-			}
-		}
+		content
 			.onAppear {
 				viewModel.startObserving()
 			}
 			.onDisappear {
 				viewModel.dispose()
 			}
+	}
+	
+	@ViewBuilder
+	private var content: some View {
+		if viewModel.totalCount == 0 {
+			NoDataView()
+		} else {
+			ScrollView {
+				VStack(spacing: 32) {
+					ZStack {
+						Pie(slices: $viewModel.data)
+						Text("games_count \(viewModel.totalCount)")
+					}
+					ChartLegend(legend: $viewModel.legend)
+				}
+			}
+		}
 	}
 }

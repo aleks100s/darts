b1ce50be9ba3ef5a2c5f8db5f043926eff6c7b44
aleks100s <aleksandr100s@yandex.ru>
2023-07-01 21:40:36 +0300

    [iOS] Add App Clip target

---
+++ iosApp/DartsAppClip/DartsAppClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  DartsAppClipApp.swift
+//  DartsAppClip
+//
+//  Created by Alexander on 01.07.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct DartsAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
